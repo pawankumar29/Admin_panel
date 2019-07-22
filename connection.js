13174ac002771aa788398d49bf350ecbc856e0fd
akash <akash.kukkar@debutinfotech.com>
2019-07-22 11:41:43 +0530

    questions csv module done

--- connection.js
+++ connection.js
@@ -466,57 +466,20 @@ var instituteWiseCategoriesSchema = new Schema({
 });
 
 var questionSchema = new Schema({
-    organisation_id: {
-        type: Schema.Types.ObjectId,
-        ref: "organisations"
-    },
-    "question": {
-        type: String,
-        default: ""
-    },
-    "category_id": {
-        type: Schema.Types.ObjectId,
-        ref: 'question_categories'
-    },
-    "sub_category_id": {
-        type: Schema.Types.ObjectId,
-        ref: 'question_categories'
-    },
-    "image": {
-        type: String,
-        default: ""
-    },
-    "options": [{
-            "id": {
-                type: Number,
-                default: 0
-            }
-        },
-        {
-            "option": {
-                type: [String],
-                default: ""
-            }
-        },
+    organisation_id: { type: Schema.Types.ObjectId, ref: "organisations" },
+    "question": { type: String, default: "" },
+    "category_id": { type: Schema.Types.ObjectId, ref: 'question_categories' },
+    "sub_category_id": { type: Schema.Types.ObjectId, ref: 'question_categories' },
+    "image": { type: String, default: "" },
+    "options": [
+        { "id": { type: String } },
+        { "is_correct": { type: Number, default: 0 } },
+        { "option": { type: [String], default: "" } },
     ],
-    "answer": {
-        type: [Number],
-        default: []
-    },
-    "status": {
-        type: Number,
-        default: 1
-    }, // 0-inactive,1-active     
-    "is_deleted": {
-        type: Number,
-        default: 0
-    }, //1-deleted by admin
-}, {
-    timestamps: {
-        createdAt: 'created_at',
-        updatedAt: 'updated_at'
-    }
-});
+    "answer": { type: [String], default: [] },
+    "status": { type: Number, default: 1 }, // 0-inactive,1-active     
+    "is_deleted": { type: Number, default: 0 }, //1-deleted by admin
+}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
 
 var quizResultScehma = new Schema({
     organisation_id: {
--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,9 +1,10 @@
 const institutes = require('../models/institutes')
 const moment = require('moment')
-// const mails = require('../helper/send_mail.js');
-// const email = require('../email_template_cms_pages');
+    // const mails = require('../helper/send_mail.js');
+    // const email = require('../email_template_cms_pages');
 const email_templates = require('../models/email_template.js')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const questionModel = mongoose.model('questions');
 const users = require("../models/user");
 const settings = require('../models/settings')
 var util = require('util');
@@ -11,7 +12,7 @@ const quizzes = require("../models/quiz")
 const quiz_results = require("../models/quiz_result")
 const institute_categories = require("../models/institute_categories")
 const question_categories = require("../models/question_categories")
-
+const Sync = require('sync');
 var multer = require('multer');
 var util = require('util');
 var fs = require('fs')
@@ -19,21 +20,20 @@ var csv = require('fast-csv');
 
 function fileFilter(req, file, cb) {
     if (
-            file.mimetype == 'text/csv' ||
-            file.mimetype == 'text/xlsx' ||
-            file.mimetype == 'text/xls'
-            ) {
+        file.mimetype == 'text/csv' ||
+        file.mimetype == 'text/xlsx' ||
+        file.mimetype == 'text/xls'
+    ) {
         cb(null, true)
     } else {
         cb(null, false)
     }
-}
-;
+};
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: function(req, file, cb) {
         cb(null, './public/csv_files')
     },
-    filename: function (req, file, cb) {
+    filename: function(req, file, cb) {
         cb(null, Date.now() + '_' + file.originalname)
     }
 });
@@ -82,82 +82,81 @@ exports.get_categoriesList = (req, res, next) => {
                 $limit: global.config.pagination_limit
             }
         ];
-        console.log(util.inspect(aggregation_query, {depth: null}));
         let p1 = question_categories.count({
             organisation_id: req.user.organisation_id,
             is_deleted: 0
         })
         let p2 = question_categories.aggregate(aggregation_query)
         Promise.all([p1, p2])
-                .then(([count, result]) => {
-//                    console.log(util.inspect(result,{depth:null}));
-                    let last = parseInt(
-                            count % global.config.pagination_limit == 0 ?
-                            count / global.config.pagination_limit :
-                            count / global.config.pagination_limit + 1
-                            )
-                    let pages = []
-                    for (i = 1; i <= last; i++) {
-                        pages.push(i)
-                    }
-                    if (req.query.page) {
-                        res.render('quiz/table', {
-                            response: result,
-                            count: count,
-                            prev: parseInt(options.page - 1 < 1 ? 1 : options.page - 1),
-                            last: last,
-                            pages: pages,
-                            next: options.page == last ? last : last + 1,
-                            message: req.flash(),
-                            options: options,
-                            current: req.query.page || 1,
-                            delta: global.config.delta,
-                            title: 'Manage Quiz',
-                            active: 'manage_quiz_page'
-                        });
-                    } else {
-                        res.render('quiz/categoriesList', {
-                            response: result,
-                            count: count,
-                            prev: parseInt(options.page - 1 < 1 ? 1 : options.page - 1),
-                            last: last,
-                            pages: pages,
-                            next: options.page == last ? last : last + 1,
-                            message: req.flash(),
-                            options: options,
-                            current: req.query.page || 1,
-                            delta: global.config.delta,
-                            title: 'Manage Quiz',
-                            active: 'manage_quiz_page'
-                        })
+            .then(([count, result]) => {
+                //                    console.log(util.inspect(result,{depth:null}));
+                let last = parseInt(
+                    count % global.config.pagination_limit == 0 ?
+                    count / global.config.pagination_limit :
+                    count / global.config.pagination_limit + 1
+                )
+                let pages = []
+                for (i = 1; i <= last; i++) {
+                    pages.push(i)
                 }
-                })
-                .catch(error => {
-                    reject(error)
-                })
+                if (req.query.page) {
+                    res.render('quiz/table', {
+                        response: result,
+                        count: count,
+                        prev: parseInt(options.page - 1 < 1 ? 1 : options.page - 1),
+                        last: last,
+                        pages: pages,
+                        next: options.page == last ? last : last + 1,
+                        message: req.flash(),
+                        options: options,
+                        current: req.query.page || 1,
+                        delta: global.config.delta,
+                        title: 'Manage Quiz',
+                        active: 'manage_quiz_page'
+                    });
+                } else {
+                    res.render('quiz/categoriesList', {
+                        response: result,
+                        count: count,
+                        prev: parseInt(options.page - 1 < 1 ? 1 : options.page - 1),
+                        last: last,
+                        pages: pages,
+                        next: options.page == last ? last : last + 1,
+                        message: req.flash(),
+                        options: options,
+                        current: req.query.page || 1,
+                        delta: global.config.delta,
+                        title: 'Manage Quiz',
+                        active: 'manage_quiz_page'
+                    })
+                }
+            })
+            .catch(error => {
+                reject(error)
+            })
     }).catch(err => {
         console.log(err)
-        //        res.redirect('/institutes');
+            //        res.redirect('/institutes');
     })
 }
 exports.add_category = (req, res, next) => {
     try {
         settings
-                .findOnePromise({}, {
-                    qualification: 1
-                })
-                .then(data => {
-                    res.render('quiz/add', {
-                        title: 'Add Category',
-                        active: 'manage_quiz_page',
-//                        qualification: data.qualification,
-                        message: req.flash()
-                    });
-                })
-                .catch(error => {
-                    reject(error)
-                })
-        // render view add institution page
+            .findOnePromise({}, {
+                qualification: 1
+            })
+            .then(data => {
+                res.render('quiz/add', {
+                    title: 'Add Category',
+                    active: 'manage_quiz_page',
+                    //                        qualification: data.qualification,
+                    message: req.flash()
+                });
+            })
+            .catch(error => {
+                reject(error)
+            })
+            // render view add institution page
     } catch (err) {
         res.render('error', {
             error: err
@@ -168,11 +167,11 @@ exports.save_new_category = (req, res, next) => {
     try {
         var sub_category = [];
         if (req.body.category_type == '1') {
-            if (typeof (req.body.sub_category) == 'string') {
-                sub_category = [{name: req.body.sub_category}]
+            if (typeof(req.body.sub_category) == 'string') {
+                sub_category = [{ name: req.body.sub_category }]
             } else {
                 for (var i = 0; i < req.body.sub_category.length; i++) {
-                    sub_category.push({name: req.body.sub_category[i]});
+                    sub_category.push({ name: req.body.sub_category[i] });
                 }
             }
 
@@ -184,28 +183,28 @@ exports.save_new_category = (req, res, next) => {
             sub_category: sub_category
         }
         question_categories.save(data).then(response => {
-            res.redirect('/quiz');
-        }).catch(err => {
-            res.render('error', {
-                error: err
+                res.redirect('/quiz');
+            }).catch(err => {
+                res.render('error', {
+                    error: err
+                })
             })
-        })
-//        settings
-//                .findOnePromise({}, {
-//                    qualification: 1
-//                })
-//                .then(data => {
-//                    res.render('quiz/add', {
-//                        title: 'Add Category',
-//                        active: 'manage_quiz_page',
-////                        qualification: data.qualification,
-//                        message: req.flash()
-//                    })
-//                })
-//                .catch(error => {
-//                    reject(error)
-//                })
-        // render view add institution page
+            //        settings
+            //                .findOnePromise({}, {
+            //                    qualification: 1
+            //                })
+            //                .then(data => {
+            //                    res.render('quiz/add', {
+            //                        title: 'Add Category',
+            //                        active: 'manage_quiz_page',
+            ////                        qualification: data.qualification,
+            //                        message: req.flash()
+            //                    })
+            //                })
+            //                .catch(error => {
+            //                    reject(error)
+            //                })
+            // render view add institution page
     } catch (err) {
         res.render('error', {
             error: err
@@ -223,7 +222,7 @@ exports.csvDowload = (req, res, next) => {
 }
 exports.importCsvCat = (req, res, next) => {
     try {
-        question_categories.findOne({_id: req.params.cat_id}).then(data => {
+        question_categories.findOne({ _id: req.params.cat_id }).then(data => {
             res.render('quiz/importQuestion', {
                 category: data,
                 title: 'Add Questions',
@@ -241,10 +240,21 @@ exports.importCsvCat = (req, res, next) => {
 }
 exports.addCsv = (req, res, next) => {
     try {
-        upload(req, res, function (err) {
+        upload(req, res, async function(err) {
             if (!err) {
-                console.log(req.body);
-                console.log(req.file);
+                let sub_category_id = '';
+                if (req.body.sub_category_id) {
+                    sub_category_id = req.body.sub_category_id
+                }
+                if (req.file) {
+                    let response = await dataUpload(req.user.organisation_id.toString(), req.body.category_id, sub_category_id, req.file.path);
+                    req.flash('success', 'Questions added successfully!!')
+                    res.redirect('/quiz')
+                } else {
+                    req.flash('error', 'Please Upload a csv file of questions details')
+                    res.redirect('/quiz')
+                }
+
             } else {
                 console.log(err)
                 res.render('error', {
@@ -261,7 +271,7 @@ exports.addCsv = (req, res, next) => {
 }
 exports.importCsvSubCat = (req, res, next) => {
     try {
-        question_categories.findOne({_id: req.params.cat_id}).then(data => {
+        question_categories.findOne({ _id: req.params.cat_id }).then(data => {
             var sub_cat_data = {};
             for (var i = 0; i < data.sub_category.length; i++) {
                 if (data.sub_category[i]._id.toString() == req.params.sub_cat_id.toString()) {
@@ -285,62 +295,94 @@ exports.importCsvSubCat = (req, res, next) => {
 
     }
 }
-function dataUpload(category_id, path) {
-    // let organisation_id = req.user.organisation_id
-    organisation_id = mongoose.Types.ObjectId(organisation_id);
-    let invalidArray = [];
-    let validArray = [];
-    let csvDataArray = [];
-    let emailArray = [];
-    let updateBatchArray = [];
-    let completePath = process.cwd() + '/' + path;
-    //    console.log(completePath);
-    var stream = fs.createReadStream(completePath)
-    csv.fromStream(stream, {
-        headers: true
-    }).on('data', function (data) {
-        csvDataArray.push(data)
-    }).on('end', function (count) {
-        if (count > 0) {
-            let questiondata = {};
-            let matchedData = {};
-            for (index = 0; index < csvDataArray.length; index++) {
-                let obj = csvDataArray[index];
-                let alreadyExist = 0;
-                if (obj["question"] && obj["image"] && obj["option1"] && obj["option2"] && obj["option3"] && obj["option4"] && obj["answer"]) {
-                    obj["options"] = [];
-                    if (obj["option1"] != "") {
-                        obj["options"].push({id: "A", option: obj["option1"]});
+
+async function dataUpload(organisation_id, category_id, sub_cat_id, path) {
+    try {
+        organisation_id = mongoose.Types.ObjectId(organisation_id);
+        let invalidArray = [];
+        let validArray = [];
+        let csvDataArray = [];
+        let completePath = process.cwd() + '/' + path;
+        var stream = fs.createReadStream(completePath)
+        csv.fromStream(stream, {
+            headers: true
+        }).on('data', function(data) {
+            csvDataArray.push(data)
+        }).on('end', async function(count) {
+            if (count > 0) {
+                for (let index = 0; index < csvDataArray.length; index++) {
+                    let answer = [];
+                    let obj = csvDataArray[index];
+                    if (obj["question"] && obj["option1"] && obj["option2"] && obj["option3"] && obj["option4"] && obj["answer"]) {
+                        obj["options"] = [];
+                        if (obj["option1"] != "") {
+
+                            if (obj["answer"].trim().toLowerCase() == "option1") {
+                                answer.push("A")
+                                obj["options"].push({ id: "A", option: obj["option1"], is_correct: 1 });
+                            } else
+                                obj["options"].push({ id: "A", option: obj["option1"], is_correct: 0 });
+                        }
+                        if (obj["option2"] != "") {
+                            if (obj["answer"].trim().toLowerCase() == "option2") {
+                                answer.push("B");
+                                obj["options"].push({ id: "B", option: obj["option2"], is_correct: 1 });
+                            } else
+                                obj["options"].push({ id: "B", option: obj["option2"], is_correct: 0 });
+
+                        }
+                        if (obj["option3"] != "") {
+                            if (obj["answer"].trim().toLowerCase() == "option3") {
+                                answer.push("C")
+                                obj["options"].push({ id: "C", option: obj["option3"], is_correct: 1 });
+                            } else
+                                obj["options"].push({ id: "C", option: obj["option3"], is_correct: 0 });
+
+                        }
+                        if (obj["option4"] != "") {
+                            if (obj["answer"].trim().toLowerCase() == "option4") {
+                                answer.push("D");
+                                obj["options"].push({ id: "D", option: obj["option4"], is_correct: 1 });
+                            } else
+                                obj["options"].push({ id: "D", option: obj["option4"], is_correct: 0 });
+
+                        }
+                        if (!obj["image"]) {
+                            obj["image"] = "";
+                        }
+                        if (answer.length) {
+                            delete obj["option1"];
+                            delete obj["option2"];
+                            delete obj["option3"];
+                            delete obj["option4"];
+                            obj["answer"] = answer;
+                            obj["organisation_id"] = organisation_id;
+                            obj["category_id"] = category_id;
+                            if (sub_cat_id != '')
+                                obj["sub_category_id"] = sub_cat_id;
+                            validArray.push(obj);
+                        } else {
+                            obj["message"] = "No Answer found";
+                            obj["errorStatus"] = 1; //field missing
+                            obj["index"] = index;
+                            invalidArray.push(obj);
+                        }
+                    } else {
+                        obj["message"] = "Fields are missing.";
+                        obj["errorStatus"] = 1; //field missing
+                        obj["index"] = index;
+                        invalidArray.push(obj);
                     }
-                    if (obj["option2"] != "") {
-                        obj["options"].push({id: "A", option: obj["option2"]});
-                    }
-                    if (obj["option3"] != "") {
-                        obj["options"].push({id: "A", option: obj["option3"]});
-                    }
-                    if (obj["option4"] != "") {
-                        obj["options"].push({id: "A", option: obj["option4"]});
-                    }
-                } else {
-                    obj["message"] = "Fields are missing.";
-                    obj["errorStatus"] = 1; //field missing
-                    obj["index"] = index;
-                    invalidArray.push(obj);
-                    continue;
                 }
+                // console.log(validArray.length);
+                // console.log(util.inspect(validArray, { depth: null }));
+                console.log("Invalid Questions  ", invalidArray.length);
+                console.log(util.inspect(invalidArray, { depth: null }));
+                await questionModel.create(validArray);
+                return Promise.resolve();
             }
-//            let promiseArray = [];
-//            
-//            
-//            Promise.all(promiseArray).then(([insert, update, updatecount]) => {
-//                return {message: "success", status: 1, errorData: invalidArray};
-//            }).catch(err => {
-//                return {message: err.message, status: 0}
-//            })
-
-
-        }
-    })
+        });
+    } catch (err) {
+        return Promise.reject(err);
+    }
 }
-
-
