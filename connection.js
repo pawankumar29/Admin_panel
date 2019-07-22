08834671126c65c80daf386867bb39975869e5a8
akash <akash.kukkar@debutinfotech.com>
2019-07-22 12:42:29 +0530

    type issue fixed

--- connection.js
+++ connection.js
@@ -471,11 +471,7 @@ var questionSchema = new Schema({
     "category_id": { type: Schema.Types.ObjectId, ref: 'question_categories' },
     "sub_category_id": { type: Schema.Types.ObjectId, ref: 'question_categories' },
     "image": { type: String, default: "" },
-    "options": [
-        { "id": { type: String } },
-        { "is_correct": { type: Number, default: 0 } },
-        { "option": { type: [String], default: "" } },
-    ],
+    "options": [{ type: Schema.Types.Mixed }],
     "answer": { type: [String], default: [] },
     "status": { type: Number, default: 1 }, // 0-inactive,1-active     
     "is_deleted": { type: Number, default: 0 }, //1-deleted by admin
